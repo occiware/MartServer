b0143986bc2f7d2c554e071b1a4ead72066667bb
Christophe Gourdin <christophe.gourdin@inria.fr>
2017-04-07 17:10:53 +0200

    [Update]Now occinterface display index.html page on jetty and tomcat but url must be complete with index.html, welcome files section doesnt work.

---
+++ start_embed_jetty.sh
@@ -0,0 +1,6 @@
+cd org.occiware.mart.war/
+## With occinterface frontend. to use the front, go to http://localhost:8080/occinterface/index.html
+mvn jetty:run-war -Pwithoccinterface
+
+## Without occinterface frontend.
+## mvn jetty:run-war
---
+++ start_embed_tomcat.sh
@@ -0,0 +1,7 @@
+cd org.occiware.mart.war/
+## With occinterface frontend. to use the front, go to http://localhost:8080/occinterface/index.html
+mvn tomcat7:run-war -Pwithoccinterface
+
+## Without occinterface frontend.
+## mvn tomcat7:run-war
+
